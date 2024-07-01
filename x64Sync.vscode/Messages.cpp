0652c09f1cc3da338b7466e4fbcdb71f7e1b2ad8
Diogo Santos <diommsantos@gmail.com>
2024-07-01 14:21:57 +0100

    Deleted some unnecessary files

--- x64Sync.vscode/Messages.cpp
+++
@@ -1,7 +0,0 @@
-#include "Messages.hpp"
-
-//manually need to add here the id and the corresponding decode function
-std::unordered_map<std::string_view, Message::Decoder> Message::decoders 
-{
-{LocM::id, LocM::decode}
-};
